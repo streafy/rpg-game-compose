785cb90d286663350f9a5acffd0a93f94308f87f
streafy <streafyhs@gmail.com>
2024-01-26 03:10:19 +0700

    migrate to version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    extra.apply {
-        set("room_version", "2.6.1")
-    }
-}
-
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.kotlin) apply false
+    alias(libs.plugins.ksp) apply false
 }
